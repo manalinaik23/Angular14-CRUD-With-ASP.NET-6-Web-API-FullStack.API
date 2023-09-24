884f5be9e3cadabdb6fb557391d6ee54c7119e8e
manalinaik71 <manalinaik71@gmail.com>
2023-09-24 14:30:13 +0530

    Add project files.

---
+++ FullStack.API/Data/FullStackDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FullStack.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace FullStack.API.Data
+{
+    public class FullStackDbContext : IdentityDbContext
+    {
+        public FullStackDbContext(DbContextOptions<FullStackDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
